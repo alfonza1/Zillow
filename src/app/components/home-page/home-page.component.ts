7e53c67edfaeef9b6e96d94a315a3b742c21f553
Alfonza <alfonzajones35@yahoo.com>
2023-06-23 19:09:51 -0400

    Rebrand starting & even more responsive on different screens

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+
+  ngOnInit() {
+    // Store the scroll position when the page is unloaded
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('scrollPosition', window.pageYOffset.toString());
+    });
+
+    // Restore the scroll position when the page is loaded
+    window.addEventListener('load', () => {
+      const scrollPosition = sessionStorage.getItem('scrollPosition');
+      if (scrollPosition) {
+        window.scrollTo(0, parseInt(scrollPosition, 10));
+        sessionStorage.removeItem('scrollPosition');
+      }
+    });
+  }
 
 }
